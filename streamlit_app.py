d76258a8632fd5a04cf08e7ca1b9584cecad671c
Grace Coppola <112916407+gcoppola19@users.noreply.github.com>
2026-08-17 17:59:59 -0500

    Update API endpoint for smoothie fruit retrieval

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,5 +40,5 @@ if ingredients_list:
         st.success('Your Smoothie is ordered, ' + name_on_order, icon="✅")
         
 import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/23](https://my.smoothiefroot.com/api/fruit/23)")  
+smoothiefroot_response = requests.get("[https://www.smoothiefroot.com/api/fruit/watermelon](https://www.smoothiefroot.com/api/fruit/watermelon")  
 st.text(smoothiefroot_response.json())
