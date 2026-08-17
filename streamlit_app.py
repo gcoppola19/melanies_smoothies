c1e5a3d66e5c9017f84f3eaa4c794e5ab573c362
Grace Coppola <112916407+gcoppola19@users.noreply.github.com>
2026-08-17 17:40:41 -0500

    Increase max ingredient selections from 5 to 6

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,5 +21,21 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:",
     my_dataframe,
-    max_selections=5
+    max_selections=6
 )
+
+if ingredients_list:
+
+    ingredients_string = ''
+
+    for chosen_fruit in ingredients_list:
+        ingredients_string += chosen_fruit + ' '
+
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+                    values ('""" + ingredients_string + """','"""+name_on_order+ """')"""
+
+    time_to_insert = st.button('Submit Order')
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success('Your Smoothie is ordered, ' + name_on_order, icon="✅")
+        
